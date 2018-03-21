139206912206b812ada82735b91fac66799f2579
Mitchell Vega <neuralnet@gmail.com>
2018-03-21 15:36:42 -0700

    docs for impactcalc
    Initializing the html and js code for impact calc, viewable at https://codepen.io/neuralnet/project/editor/DYrnBW

--- tempy.js
+++ tempy.js
@@ -5,7 +5,18 @@ CREDIT SCORE IMPACT CALCULATOR
 
 Visa 5257  - 12.15% APR - $500 limit - 6 age
 
-(){
+
+
+
+
+
+
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+console.log("JavaScript is amazing!");
 
 
 	function CardConstruct(lastdigs, assoc, apr, limit, age) {
@@ -16,7 +27,45 @@ Visa 5257  - 12.15% APR - $500 limit - 6 age
 		this.age = age;
 	}
 
-	let myPurpleVisa = new CardConstruct(5257 , "Visa" , 12.15 , 500 , 2190);
+	let myPurpleVisa = new CardConstruct(5257, "Visa", 12.15, 500, 2190);
 
 }
 
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <!--  Meta  -->
+  <meta charset="UTF-8" />
+  <title>My New Pen!</title>
+  
+  <!--  Styles  -->
+  <link rel="stylesheet" href="styles/index.processed.css">
+</head>
+<body>
+  
+<form>
+  <input name="lastDigs" value="0000">Last 4 Digits</input>
+  <select name="assoc">
+    <option value="amex">American Express</option>
+    <option value="discover">Discover</option>
+    <option value="mc">MasterCard</option>
+    <option value="sb">Store Brand</option>
+    <option value="visa">Visa</option>
+  </select>
+  <button>Add Card</button>
+  </form>
+  
+  <!-- Scripts -->
+  <script src="scripts/index.js"></script>
+</body>
+</html>
