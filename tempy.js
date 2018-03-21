3325cb21a105dfc10bd52810d785f7c279471b8d
Mitchell Vega <neuralnet@gmail.com>
2018-03-21 14:58:09 -0700

    staging some stuff for my impact calculator

---
+++ tempy.js
@@ -0,0 +1,22 @@
+
+
+
+CREDIT SCORE IMPACT CALCULATOR
+
+Visa 5257  - 12.15% APR - $500 limit - 6 age
+
+(){
+
+
+	function CardConstruct(lastdigs, assoc, apr, limit, age) {
+		this.lastDigs = lastdigs;
+		this.assoc = assoc;
+		this.apr = apr;
+		this.limit = limit;
+		this.age = age;
+	}
+
+	let myPurpleVisa = new CardConstruct(5257 , "Visa" , 12.15 , 500 , 2190);
+
+}
+
